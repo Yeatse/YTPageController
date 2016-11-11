67532477f9c90a71a77075be5290d0084118231a
Taras Palienko <palienko.t@dbbest.com>
2016-11-11 18:30:17 +0200

    fix viewWillAppear not being called on view controllers issue

---
+++ Example/YTPageController/Segmented Control Transition/YTFirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  YTFirstViewController.h
+//  YTPageController
+//
+//  Created by tar on 11.11.16.
+//  Copyright © 2016 Yeatse CC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+/*!
+ @class YTFirstViewController is used to debug view controller lifecycle.
+ */
+@interface YTFirstViewController : UIViewController
+
+@end
